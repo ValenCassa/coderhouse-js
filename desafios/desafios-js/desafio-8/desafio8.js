74d05a02d5637ee685dbf1daa802962de8d0d1b1
ValenCassa <valencassa@gmail.com>
2021-11-30 12:09:08 -0300

    added desafios

--- desafios/desafios-js/desafio-8/desafio8.js
+++ desafios/desafios-js/desafio-8/desafio8.js
@@ -1,16 +1,24 @@
-var button = document.getElementById('btn-calc');
-
 var cuotasMes = [];
+let datainfla = 0
 
-button.onclick = function start() {
+$.getJSON("https://apis.datos.gob.ar/series/api/series/?ids=173.1_INUCLEOLEO_DIC-_0_10&limit=5000&format=json", (data) => {
+    datainfla = ((data.data.slice(-1)[0][1]) * 100).toFixed(1)
+    
+
+})
+setTimeout(function(){
+    
+    document.getElementById('infla').textContent = datainfla + '%';
+    document.getElementById('inflacion').setAttribute('placeholder', datainfla)
+}, 100)
+
+
+
+$('#href').click(function () {
 
     function isNumeric(num) {
         return !isNaN(parseFloat(num)) && isFinite(num);
       };
-
-
-
-    
     
         class Cuotas {
             constructor (valor_inicial, valor_final, cantidad, inflacion, cuotas_mes) {
@@ -58,13 +66,6 @@ button.onclick = function start() {
             go = true;
             let valor_hoy = 0;
 
-            /*
-            for (let i = 0; i < info.cantidad; i++) {
-                valor_hoy += Math.round( info.cuotas_mes - (info.cuotas_mes * info.inflacion));
-                info.cuotas_mes -= info.cuotas_mes * info.inflacion;
-            };
-
-            */
             info.valor_final = info.valor_final / Math.pow((1 + info.inflacion), 1);
 
             for (let i = 0; i < info.cantidad; i++) {
@@ -79,15 +80,22 @@ button.onclick = function start() {
 
             localStorage.setItem('cuotasMes', JSON.stringify(cuotasMes));
             localStorage.setItem('info', JSON.stringify(info));
-            document.getElementById('href').onclick(window.location.href='/coderhouse-js/desafios/desafios-html/desafio-8/desafio-resultado.html')
+            $('#href').click(window.location.href='/coderhouse-js/desafios/desafios-html/desafio-8/desafio-resultado.html');
             
             
         }else {
             alert('Ingresa un valor valido');
             };
+});
 
-            
+function enter(parent) {
+    $(parent).animate({marginLeft: '-8em'}), 2000, $(parent).animate({marginLeft:'-4em'}, 1500, function(){
+        enter(parent);
+    })
 };
+
+document.getElementById('href').addEventListener('mouseenter', enter('#arrow'))
+
 
 
 
